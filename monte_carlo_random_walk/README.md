4ecbf5e095c3f40e8f8d854a67dd9ada54eb27d3
sammanthp007 <sammanbikramthapa@gmail.com>
2017-10-26 16:38:51 -0400

    update README for final

--- monte_carlo_random_walk/README.md
+++ monte_carlo_random_walk/README.md
@@ -4,5 +4,8 @@ Create a random walk program that tries to minimize the amount of money a user s
 
 Simulation is written in Python.
 
+When we can walk for 6 block for free, and cost of transportation costs $10,
+then, on average, the most amount of blocks you can walk with the least
+amount of money seems to be 7 blocks with $867 or 9 blocks with $1636 for 20000
+walks.
 
-Most number of random walks that one can take for free is walking randomly for 44 blocks.
--- monte_carlo_random_walk/random_walk.py
+++ monte_carlo_random_walk/random_walk.py
@@ -16,7 +16,7 @@ def main():
     """ First function that runs on running this module"""
     num_of_walks = 20000
     walkable_block = 5
-    max_block = 51
+    max_block = 71
     cost_of_lyft = 10
 
     last_val_more_than_50 = walkable_block
@@ -50,6 +50,7 @@ def main():
     print ("Last walk that had more chance of walking back than taking lyft ",
             last_val_more_than_50)
 
+    print "An array of cost per block and the len of walk: "
     print (sorted(cost_by_walk)[:10])
 
 main()
